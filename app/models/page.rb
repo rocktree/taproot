a361b039be22b878eb2e1efb04d2a4faa9a81475
Sean C Davis <scdavis41@gmail.com>
2015-08-11 11:34:16 -0400

    switch out concerns with superslug

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,24 +2,17 @@
 #
 # Table name: pages
 #
-#  id               :integer          not null, primary key
-#  template_id      :integer
-#  title            :string(255)
-#  slug             :string(255)
-#  description      :text
-#  body             :text
-#  ancestry         :string(255)
-#  published        :boolean          default(FALSE)
-#  field_data       :text
-#  created_at       :datetime
-#  updated_at       :datetime
-#  position         :integer          default(0)
-#  old_template_ref :string(255)
-#  order            :string(255)
-#  show_in_nav      :boolean          default(TRUE)
-#  body_md          :text
-#  page_path        :string(255)
-#  last_editor_id   :integer
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  slug       :string(255)
+#  body       :text
+#  ancestry   :string(255)
+#  published  :boolean          default(FALSE)
+#  field_data :text
+#  created_at :datetime
+#  updated_at :datetime
+#  position   :integer          default(0)
+#  page_path  :string(255)
 #
 
 require 'active_support/inflector'
